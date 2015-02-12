88353dafb2113a3773a740ce34492d13e2784e56
Stephan Pavlovic <stephan@railslove.com>
2015-02-12 23:23:05 +0100

    add map

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,4 +10,29 @@ angular.module('starter.controllers', [])
 
 .controller('PackageCtrl', function($scope, $stateParams, Package) {
   $scope.package = Package.get({id: $stateParams.packageId});
+
+  $scope.$on("$stateChangeSuccess", function() {
+    $scope.map = {
+      defaults: {
+        tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+        maxZoom: 15,
+        zoomControlPosition: 'topleft'
+      },
+      markers : {},
+      events: {
+        map: {
+          enable: ['context'],
+          logic: 'emit'
+        }
+      },
+      center: {
+        lat: 51.505,
+        lng: -0.09,
+        zoom: 10
+      }
+    };
+    $scope.map.center = {lat: $scope.package['start_lat'], lng: $scope.package['start_long'], zoom: 12}
+    console.log($scope.map.center);
+  });
+
 })
